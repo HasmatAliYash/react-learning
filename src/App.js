33e7332d7b4fcaf94fd6215c35d30a9629b4e87b
Hasmat Ali <hasmat.ali@yash.com>
2023-01-31 02:01:54 +0530

    counter assignment of day3 is done

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
 
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { useState } from 'react';
 import './App.css';
-import LivingThings from './components/LivingThings';
-
-
-
+import ChildComp from './components/ChildComp';
 
 function App() {
+  const [counter, setCounterNumber] = useState(0);
+
+  const increamnerHandler = () => {
+    setCounterNumber(counter + 1);
+  }
+
+  const resetHandler = () => {
+    setCounterNumber(0);
+  }
   return (
     <div className="container">
-      <LivingThings />
+      <ChildComp increamnerHandler={increamnerHandler} resetHandler={resetHandler} counter={counter} />
     </div>
   )
 }
---
+++ src/components/ChildComp.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Button from "react-bootstrap/Button";
+
+export default function ChildComp(props) {
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "space-around",
+        alignItems: "center",
+      }}
+    >
+      <Button onClick={props.increamnerHandler}>Click here</Button>
+      <h3>Counter is {props.counter}</h3>
+      <Button onClick={props.resetHandler}>Reset</Button>
+    </div>
+  );
+}
